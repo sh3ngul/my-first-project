1cb8f7c406fc57f3d92f778ea9add32286cd3cbb
Sumeyye Wengul <sumuw7@gmail.com>
2020-08-05 18:17:18 +0400

    my first commit

---
+++ cardbox.js
@@ -0,0 +1,30 @@
+// let cards = [{
+//    name: "Qush novu",
+//    photo: "qush.jpg",
+//    description: "Nadir, sayı azalmaqda olan, qışlayan və köçəri növdür. ",
+// },
+// {
+//    name: "Qush novu",
+//    photo: " qush.jpg",
+//    description: "Nadir, sayı azalmaqda olan, qışlayan və köçəri növdür. ",
+// },
+// {
+//    name: "Qush novu",
+//    photo: "qush.jpg",
+//    description: "Nadir, sayı azalmaqda olan, qışlayan və köçəri növdür. ",
+// },
+// {
+//    name: "Qush novu",
+//    photo: "qush.jpg",
+//    description: "Nadir, sayı azalmaqda olan, qışlayan və köçəri növdür. ",
+// },
+// {
+//    name: "Qush novu",
+//    photo: "qush.jpg",
+//    description: "Nadir, sayı azalmaqda olan, qışlayan və köçəri növdür. ",
+// },
+// {
+//    name: "Qush novu",
+//    photo: "qush.jpg",
+//    description: "Nadir, sayı azalmaqda olan, qışlayan və köçəri növdür. ",
+// }];
